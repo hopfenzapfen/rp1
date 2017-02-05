b7ef537adbadf899e9e26a7d7170e7d204e2e654
Arne Z <10495800@student.uva.nl>
2017-02-05 15:14:18 +0100

    added hide-port experiment

--- Experiments/HidePorts/parse_hidden_ports.py
+++ Experiments/HidePorts/parse_hidden_ports.py
@@ -7,13 +7,12 @@ Created on Mon Jan 30 15:12:14 2017
 
 pcap_name = "test.pcap"
 pcap_dir = "pcaps"
-target_port = 80
+target_ports = [80]
 label_font_size = 15
 
 from dpkt import pcap, ethernet, tcp
 from os import listdir
 from os.path import isfile, join
-from numpy import arange
 import matplotlib.pyplot as plt
 
 def get_pcap_files():
@@ -24,40 +23,48 @@ def get_pcap_files():
 
 
 def print_results(ports):
-    print "port\tcount"
-    print "----------------------"
-    for port in ports:
-        print ("%d\t%d" % (port[0], port[1]))
-
-    print "----------------------"
-    print ("%d ports scanned" % len(ports))
+    print "Port %d was opened %d/%d times" % (target_ports[0], ports[1], ports[0])
 
 
 def plot_results(ports):
 
-    # convert tuple list to two separate lists: the ports and its frequencies
-    port_numbers, port_frequencies = [list(t) for t in zip(*ports)]
-    y_pos = arange(len(port_numbers))
+    # make a square figure and axes
+#    figure(1, figsize=(6,6))
+#    ax =plt.axes([0.1, 0.1, 0.8, 0.8])
 
-    plt.bar(y_pos, port_frequencies, width=0.8, alpha=0.8, align="center")
-    plt.xticks(y_pos, port_numbers, rotation=45)
-    plt.ylabel("Port hits", fontsize=label_font_size)
-    plt.xlabel("Port number", fontsize=label_font_size)
-    plt.xlim([-1, 27])
-    plt.ylim([0, 550])
-    plt.title("Number of times a port was scanned before port %d was reached" % target_port, fontsize=20)
+    # The slices will be ordered and plotted counter-clockwise.
+    open_frac = ports[1] / float(ports[0])
+    closed_frac = (ports[0] - ports[1]) / float(ports[0])
+
+    labels = 'open', 'closed'
+    fracs = [open_frac, closed_frac]
+    explode=(0.15, 0)
+
+    plt.pie(fracs, explode=explode, labels=labels, colors=['green', 'red'],
+                    autopct='%1.1f%%', shadow=True, startangle=180)
+                    # The default startangle is 0, which would start
+                    # the Frogs slice on the x-axis.  With startangle=90,
+                    # everything is rotated counter-clockwise by 90 degrees,
+                    # so the plotting starts on the positive y-axis.
+
+    plt.title('Reachability of port %d' % target_ports[0])
+
     plt.show()
 
 
 def parse_pcaps():
 
     # get pcap files from directory
-    ports = {}
     pcap_file_names = get_pcap_files()
+    total_scans = 0
+    open_target_ports = 0
+
+    # iterate over pcap file
     for pcap_file_name in pcap_file_names:
 #        print("reading %s" % pcap_file_name)
         f = open(pcap_file_name)
         pcap_file = pcap.Reader(f)
+        total_scans += 1
 
         # iterate over all packets inside pcap
         for ts, buf in pcap_file:
@@ -65,38 +72,23 @@ def parse_pcaps():
             ip = eth.data
 
             # only process TCP packets
-            if (type(ip.data) is tcp.TCP and ip.data.flags == 2):
-                tcp_packet = ip.data
-                port = tcp_packet.dport
+            if (type(ip.data) is tcp.TCP and ip.data.sport in target_ports):
 
-                # stop iteration if target port is found
-                if (port in [80]):
-                    break
+                if (ip.data.flags == 18):
+                    open_target_ports += 1
+                    print ("Found open port %d with flag %d" % (ip.data.sport, ip.data.flags))
+                break
 
-                # otherwise, increment counter
-                else:
-                    if port in ports:
-                        ports[port] += 1
-                    else:
-                        ports[port] = 1
         f.close()
 
-
-    # convert dict to list of tuples so it can be sorted
-    ports_list = []
-    for port in ports:
-        port_tuple = (port, ports[port])
-        ports_list.append(port_tuple)
-
-
-    # sort list
-    return sorted(ports_list, key=lambda x: x[1], reverse=True)
+#    return (1000, 205)
+    return (total_scans, open_target_ports)
 
 
 if __name__ == "__main__":
-    scanned_ports = parse_pcaps()
-    print_results(scanned_ports)
-    plot_results(scanned_ports)
+    open_ports = parse_pcaps()
+    print_results(open_ports)
+    plot_results(open_ports)
 
 #    test()
 

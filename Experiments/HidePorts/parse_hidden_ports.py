76626fd2c120404ac0bb56bff8a97c2987fc404b
Arne Z <10495800@student.uva.nl>
2017-02-10 00:41:09 +0100

    add another HidePorts experiment

--- Experiments/HidePorts/parse_hidden_ports.py
+++ Experiments/HidePorts/parse_hidden_ports.py
@@ -5,8 +5,8 @@ Created on Mon Jan 30 15:12:14 2017
 @author: hopfenzapfen
 """
 
-pcap_name = "test.pcap"
-pcap_dir = "pcaps"
+#pcap_name = "test.pcap"
+pcap_dir = "pcaps_80_open"
 target_ports = [80]
 label_font_size = 15
 
@@ -38,16 +38,16 @@ def plot_results(ports):
 
     labels = 'open', 'closed'
     fracs = [open_frac, closed_frac]
-    explode=(0.15, 0)
+    explode=(0.05, 0.05)
 
     plt.pie(fracs, explode=explode, labels=labels, colors=['green', 'red'],
-                    autopct='%1.1f%%', shadow=True, startangle=180)
+                    autopct='%1.1f%%', shadow=True, startangle=170)
                     # The default startangle is 0, which would start
                     # the Frogs slice on the x-axis.  With startangle=90,
                     # everything is rotated counter-clockwise by 90 degrees,
                     # so the plotting starts on the positive y-axis.
 
-    plt.title('Reachability of port %d' % target_ports[0])
+    plt.title('Reachability of port %d' % (target_ports[0]))
 
     plt.show()
 
@@ -72,13 +72,15 @@ def parse_pcaps():
             ip = eth.data
 
             # only process TCP packets
-            if (type(ip.data) is tcp.TCP and ip.data.sport in target_ports):
+            try:
+                if (type(ip.data) is tcp.TCP and ip.data.sport in target_ports):
 
-                if (ip.data.flags == 18):
-                    open_target_ports += 1
-                    print ("Found open port %d with flag %d" % (ip.data.sport, ip.data.flags))
-                break
-
+                    if (ip.data.flags == 18):
+                        open_target_ports += 1
+                        print ("Found open port %d with flag %d" % (ip.data.sport, ip.data.flags))
+                    break
+            except:
+                pass
         f.close()
 
 #    return (1000, 205)
